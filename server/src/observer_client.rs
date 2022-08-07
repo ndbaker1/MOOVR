fdf03717362b2faf699588fa2de1a2bc2bf8d630
Nick Baker <ndbaker1@outlook.com>
2022-08-06 23:12:37 -0500

    Revert to Hashmap collector from Vec

--- server/src/observer_client.rs
+++ server/src/observer_client.rs
@@ -16,12 +16,11 @@ impl ObserverClientHandler {
     pub fn run(data: Arc<Mutex<ServerState>>, observers: Arc<Mutex<Vec<WebSocket<TcpStream>>>>) {
         thread::spawn(move || loop {
             if let Ok(data) = data.lock() {
-                let data_vec: Vec<_> = data.iter().map(|(_, val)| val).collect();
                 // don't send empty messages to the users
                 if !data.is_empty() {
                     observers.lock().unwrap().retain_mut(|observer| {
                         if let Err(e) = observer
-                            .write_message(Message::text(serde_json::to_string(&data_vec).unwrap()))
+                            .write_message(Message::text(serde_json::to_string(&*data).unwrap()))
                         {
                             log::error!("observer error [{}]", e);
                             return false;

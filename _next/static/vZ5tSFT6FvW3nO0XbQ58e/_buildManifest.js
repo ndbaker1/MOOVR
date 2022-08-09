09b932ac203c6c1870adaa835854434a26ba8a6c
Nick Baker <ndbaker1@outlook.com>
2022-08-09 06:11:33 +0000

    Updates

--- _next/static/vZ5tSFT6FvW3nO0XbQ58e/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-56d1cae5116bee4d.js","static/chunks/pages/index-f310c6eb1aa32104.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

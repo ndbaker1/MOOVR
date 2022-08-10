d397c7011d3d6ca13b00203ded8d73fe30c1cdc5
ndbaker1 <ndbaker1@outlook.com>
2022-08-10 03:08:00 +0000

    Deploying to gh-pages from @ ndbaker1/phone-pong@00d7c724310eab77044476d2f3bc151066c1aad6 🚀

--- _next/static/hE8TNaHwwYQdRES4eJ2RJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-56d1cae5116bee4d.js","static/chunks/702-d5c83213403a4271.js","static/chunks/pages/index-e19830ad14f28b3d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

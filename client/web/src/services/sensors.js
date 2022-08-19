e354577e7ed7098acc9c2f8f4e6ae281dfce6711
Nick Baker <ndbaker1@outlook.com>
2022-08-18 22:30:14 -0500

    Update permission checks in sensor code

--- client/web/src/services/sensors.js
+++ client/web/src/services/sensors.js
@@ -1,38 +1,45 @@
 const frequency = 60;
 
 export async function startOrientationTracker(sendCallback) {
+    if (!(await Promise.all([
+        navigator.permissions.query({ name: "accelerometer" }),
+        navigator.permissions.query({ name: "magnetometer" }),
+        navigator.permissions.query({ name: "gyroscope" }),
+    ])).every(permissionResult => permissionResult.state === 'granted')) {
+        throw new Error('failed to get permission for orientation sensors');
+    }
+
     // eslint-disable-next-line no-undef
     const sensor = new AbsoluteOrientationSensor({
         frequency,
         referenceFrame: 'device',
     });
-    const permissionResults = await Promise.all([
-        navigator.permissions.query({ name: "accelerometer" }),
-        navigator.permissions.query({ name: "magnetometer" }),
-        navigator.permissions.query({ name: "gyroscope" }),
-    ]);
 
     sensor.addEventListener('reading', () => {
         try {
             sendCallback({ type: "Rotation", data: sensor.quaternion });
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             sensor.stop();
         }
     });
     sensor.addEventListener('error', console.log);
 
-    if (permissionResults.every((result) => result.state === "granted")) {
-        sensor.start();
-    } else {
-        console.log("No permissions to use AbsoluteOrientationSensor.");
-    }
+    sensor.start();
 
     return () => sensor.stop();
 }
 
 
 export async function startAccelerometer(sendCallback) {
+    if (!(await Promise.all([
+        navigator.permissions.query({ name: "accelerometer" }),
+        navigator.permissions.query({ name: "magnetometer" }),
+        navigator.permissions.query({ name: "gyroscope" }),
+    ])).every(permissionResult => permissionResult.state === 'granted')) {
+        throw new Error('failed to get accelerometer');
+    }
+
     // eslint-disable-next-line no-undef
     let sensor = new LinearAccelerationSensor({
         frequency
@@ -41,7 +48,7 @@ export async function startAccelerometer(sendCallback) {
     sensor.addEventListener('reading', () => {
         try {
             sendCallback({ type: "Acceleration", data: [sensor.x, sensor.y, sensor.z] });
-        } catch(e) {
+        } catch (e) {
             console.error(e);
             sensor.stop();
         }

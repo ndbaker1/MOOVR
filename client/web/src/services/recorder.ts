7342bf1e5db121189809e8d8db17b602e4049935
Nick Baker <ndbaker1@outlook.com>
2022-10-10 23:17:35 -0500

    implement recorder for webcam

---
+++ client/web/src/services/recorder.ts
@@ -0,0 +1,23 @@
+export class Recorder {
+
+    public static async capture(callback: (_: ImageData) => any, frameRate = 30) {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d')!; // we are sure that we can obtain the context
+        const video = document.createElement('video');
+
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { frameRate } });
+        video.srcObject = stream;
+        video.play();
+
+        const interval = setInterval(() => {
+            ctx.drawImage(video, 0, 0);
+            callback(ctx.getImageData(0, 0, canvas.width, canvas.height));
+        }, 1000 / frameRate);
+
+        return () => {
+            clearInterval(interval);
+            canvas.remove();
+            video.remove();
+        };
+    }
+}
